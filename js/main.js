312fcac36a6e8fcbd915e7ec44fda81586bcb2ce
Yassine El hassani <yassine.ds.404@gmail.com>
2024-11-11 10:51:00 +0100

    save all edits

--- js/main.js
+++ js/main.js
@@ -1,38 +1,198 @@
-const prevBtns = document.querySelectorAll(".btn-prev");
-const nextBtns = document.querySelectorAll(".btn-next");
+const prevBtn = document.querySelectorAll(".btn-prev");
+const nextBtn1 = document.querySelectorAll(".btn-next1");
+const nextBtn2 = document.querySelectorAll(".btn-next2");
+const choice1 = document.querySelectorAll(".choice1");
+const choice2 = document.querySelectorAll(".choice2");
+const choice3 = document.querySelectorAll(".choice3");
+const choice4 = document.querySelectorAll(".choice4");
+const nextBtn4 = document.querySelectorAll(".btn-next4");
 const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
 const progressSteps = document.querySelectorAll(".progress-step");
 
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let phone = document.getElementById("phone");
-let depart = document.getElementById("depart");
-let finish = document.getElementById("finish");
-let date = document.getElementById("date");
-let choice1 = document.getElementById("c1");
-let choice2 = document.getElementById("c2");
-let choice3 = document.getElementById("c3");
-let choice4 = document.getElementById("c4");
+let nameInput = document.getElementById("name");
+let emailInput = document.getElementById("email");
+let phoneInput = document.getElementById("phone");
+let departInput = document.getElementById("depart");
+let finishInput = document.getElementById("finish");
+let dateInput = document.getElementById("date");
+let c1 = document.getElementById("c1");
+let c2 = document.getElementById("c2");
+let c3 = document.getElementById("c3");
+let c4 = document.getElementById("c4");
+let place1Input = document.getElementById("place1");
+let place2Input = document.getElementById("place2");
+let place3Input = document.getElementById("place3");
+let place4Input = document.getElementById("place4");
+let place5Input = document.getElementById("place5");
+let place6Input = document.getElementById("place6");
+let place7Input = document.getElementById("place7");
+let place8Input = document.getElementById("place8");
 
-// Next & Prev buttons
+let countAdult = document.getElementById("countAdult");
+let CountKids = document.getElementById("CountKids");
+let countA = 1;
+let countK = 1;
 
-let formStepsNum = 0;
+let total = document.getElementById("totalPrix");
+
+// countPerson functions
+function addCountAdult() {
+  if(countA >= 1) {
+    countA++;
+  } else {
+    countAdult.reset();
+  }
+  countAdult.innerHTML = `${countA}`;
+}
+
+function minCountAdult() {
+  if(countA <= 1) {
+    countAdult.reset();
+  } else {
+    countA--;
+  }
+  countAdult.innerHTML = `${countA}`;
+}
+
+function addCountKids() {
+  if(countK >= 1) {
+    countK++;
+  } else {
+    countKids.reset();
+  }
+  countKids.innerHTML = `${countK}`;
+}
+
+function minCountKids() {
+  if(countK <= 1) {
+    countKids.reset();
+  } else {
+    countK--;
+  }
+  countKids.innerHTML = `${countK}`;
+}
+
+let adultPrix = 500 * countA;
+let kidsPrix = 100 * countK;
 
 
-nextBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
+total = adultPrix + kidsPrix;
+
+// console.log("this total: " + total);
+
+
+// setUp all Next buttons
+function getTheInfo() {
+  return {
+    name: nameInput.value,
+    email: emailInput.value,
+    phone: phoneInput.value
+  }
+}
+
+function getVolsInfo() {
+  return {
+    depart: departInput.value,
+    finish: finishInput.value,
+    date: dateInput.value
+  }
+}
+
+function getThePlaces() {
+  return {
+    place1: place1Input.value,
+    place2: place2Input.value,
+    place3: place3Input.value,
+    place4: place4Input.value,
+    place5: place5Input.value,
+    place6: place6Input.value,
+    place7: place7Input.value,
+    place8: place8Input.value
+  }
+}
+
+var infoData, volsData, flightData, placesData;
+
+nextBtn1.forEach(btn => {
+  btn.addEventListener('click', () => {
+    infoData = getTheInfo();
+    console.log(infoData);
     formStepsNum++;
     updateFormSteps();
-    updateProgressbar();
+    updateProgressBar();
   });
 });
 
-prevBtns.forEach((btn) => {
+nextBtn2.forEach(btn => {
+  btn.addEventListener('click', () => {
+    volsData = getVolsInfo();
+    console.log(volsData);
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressBar();
+  });
+});
+
+choice1.forEach(btn => {
+  btn.addEventListener('click', () => {
+    flightData = "flight1";
+    console.log(flightData);
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressBar();
+  });
+});
+
+choice2.forEach(btn => {
+  btn.addEventListener('click', () => {
+    flightData = "flight2";
+    console.log(flightData);
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressBar();
+  });
+});
+
+choice3.forEach(btn => {
+  btn.addEventListener('click', () => {
+    flightData = "flight3";
+    console.log(flightData);
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressBar();
+  });
+});
+
+choice4.forEach(btn => {
+  btn.addEventListener('click', () => {
+    flightData = "flight4";
+    console.log(flightData);
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressBar();
+  });
+});
+
+nextBtn4.forEach(btn => {
+  btn.addEventListener('click', () => {
+    placesData = getThePlaces();
+    console.log(placesData);
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressBar();
+  });
+});
+
+// Prev buttons & task progress
+
+let formStepsNum = 0;
+
+prevBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     formStepsNum--;
     updateFormSteps();
-    updateProgressbar();
+    updateProgressBar();
   });
 });
 
@@ -45,7 +205,7 @@ function updateFormSteps() {
   formSteps[formStepsNum].classList.add("form-step-active");
 }
 
-function updateProgressbar() {
+function updateProgressBar() {
   progressSteps.forEach((progressStep, idx) => {
     if (idx < formStepsNum + 1) {
       progressStep.classList.add("progress-step-active");
@@ -59,98 +219,3 @@ function updateProgressbar() {
   progress.style.width =
     ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 }
-
-// dev selector
-
-(function () {
-    selectItems = document.querySelectorAll('.selectable');
-    selectItems.forEach(function(el) {
-      el.addEventListener('click', handleClick, false);
-    });
-    
-    function handleClick(e) {
-      e.target.classList.toggle('is-selected');
-    }
-})();
-  
-  
-(function () {
-    var dragbox = document.createElement('div');
-    var isDragCancelled = false;
-    var startX, startY
-    var selectItems;
-    
-    document.body.appendChild(dragbox);
-    
-    document.addEventListener('selectstart', function (e) {
-      e.preventDefault();
-    });
-    
-    document.addEventListener('mousedown', function (e) {
-      isDragCancelled = false;
-      startX = e.pageX;
-      startY = e.pageY;
-      
-      selectItems = document.querySelectorAll('.selectable');
-      
-      selectItems = Array.prototype.map.call(selectItems, function(element) {
-        return {
-          element: element,
-          isSelected: element.classList.contains('is-selected')
-        };
-      });
-      
-      document.addEventListener('mouseup', handleMouseUp);
-        setTimeout(function () {
-          if (isDragCancelled) {
-            return;
-          }
-          
-          document.addEventListener('mousemove', handleMouseMove);
-        }, 100);
-      });
-    
-    function handleMouseMove(e) {
-      var x = e.pageX;
-      var y = e.pageY;
-      var top = Math.min(startY, y);
-      var left = Math.min(startX, x);
-      var width = Math.abs(startX - x);
-      var height = Math.abs(startY - y);
-      
-      dragbox.setAttribute('style',
-        'position: absolute;' +
-        'border: 1px solid rgba(166, 193, 255, .8);' +
-        'background: rgba(166, 193, 255, .3);' +
-        'left:' + left + 'px;' + 'top:' + top + 'px;' +
-        'width:' + width + 'px;' + 'height:' + height + 'px;'
-      );
-      
-      selectItems.forEach(function (selectItem) {
-        var el = selectItem.element;
-        
-        if (
-          el.offsetLeft >= left
-          && el.offsetTop >= top
-          && el.offsetLeft + el.offsetWidth <= left + width
-          && el.offsetTop + el.offsetHeight <= top + height
-        ) {
-          selectItem.isSelected
-          ? el.classList.remove('is-selected')
-          : el.classList.add('is-selected');
-        } else {
-          selectItem.isSelected
-          ? el.classList.add('is-selected')
-          : el.classList.remove('is-selected');
-        }
-      });
-    }
-    
-    function handleMouseUp() {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-      isDragCancelled = true;
-      dragbox.style.display = 'none';
-      selectItems = [];
-    }
-  })();
